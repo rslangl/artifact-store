514b286e9ff74781a6ed6c6ccc53130aed7c436c
rslangl <fjellape1447@protonmail.com>
2026-04-17 09:17:46 +0200

    chore: add test case for config reader

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,16 +2,18 @@ package config
 
 import(
 	"os"
-	"log"
+	//"log"
 	"fmt"
+
+	"go.yaml.in/yaml/v4"
 )
 
 type ConfigStorage struct {
-	Enabled []string
+	Enabled []string `yaml:,enabled-storage-types`
 }
 
 type Config struct {
-	StorageConfig ConfigStorage
+	StorageConfig ConfigStorage `yaml:config-storage`
 }
 
 func (cfg *Config) Create(path string) error {
@@ -19,10 +21,15 @@ func (cfg *Config) Create(path string) error {
 		data, err := os.ReadFile(path)
 		if err != nil {
 			return fmt.Errorf("error occurred while reading config file '%v': %v", path, err)
+		} else {
+			err := yaml.Unmarshal(data, &cfg)
+			if err != nil {
+				return fmt.Errorf("error occurred while constructing config from file '%v': %v", path, err)
+			}
 		}
-		log.Println("%v", data)
+		//log.Println("%+v\n", data)
 	} else {
-		cfg.StorageConfig = ConfigStorage{Enabled: []string{"fs","nas","s3"}}
+		cfg.StorageConfig = ConfigStorage{Enabled: []string{"fs"}}
 	}
 	return nil
 }
---
+++ internal/config/config_test.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"testing"
+)
+
+func isEqual(v1, v2 []string) bool {
+	if len(v1) != len(v2) {
+		return false
+	}
+	for idx := range v1 {
+		if v1[idx] != v2[idx] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestCreate(t *testing.T) {
+
+	cfg := &Config{}
+
+	tests := []struct{
+		// The argument passed to the function
+		path string
+		// The expected output
+		expected Config
+		// Whether we want this test to fail
+		wantErr bool
+	}{
+		// Test 1: no path, read defaults
+		{
+			"",
+			Config{
+				StorageConfig: ConfigStorage{
+					Enabled: []string{"fs"},
+				},
+			},
+			false,
+		},
+		// Test 2: path provided, read contents
+	// 	{
+	// 		"test_config.yaml"
+	// 		Config{},
+	// 		false,
+	// 	},
+	}
+
+	for _, testInput := range tests {
+		t.Run(testInput.path, func(t *testing.T) {
+			err := cfg.Create(testInput.path)
+
+			if (err != nil) != testInput.wantErr {
+				t.Errorf("expected error: %v, got error: %v", testInput.wantErr, err)
+			}
+
+			if !isEqual(cfg.StorageConfig.Enabled, testInput.expected.StorageConfig.Enabled) {
+				t.Errorf("expected %v, got %v", testInput.expected.StorageConfig.Enabled, cfg.StorageConfig.Enabled)
+			}
+		})
+	}
+}
