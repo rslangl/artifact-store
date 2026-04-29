0767c1f253cff34ba97ce253ba69b34e917ac2aa
rslangl <151566232+rslangl@users.noreply.github.com>
2026-04-29 08:45:33 +0200

    feat: write chart through post request (#20)
    * feat: write chart through post request
    
    * chore: construct intermittent intsances using generated types
    
    * chore: remove unused helper
    
    * chore: ensure filesystem backend writer creates file

--- internal/api/impl.go
+++ internal/api/impl.go
@@ -3,8 +3,9 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-	//"path"
+	openapi_types "github.com/oapi-codegen/runtime/types"
 
 	"artifact-store/internal/storage"
 	"artifact-store/internal/storage/storage_error"
@@ -75,8 +76,79 @@ func (Server) GetChartVersions(w http.ResponseWriter, r *http.Request, name stri
 	_ = json.NewEncoder(w).Encode(res)
 }
 
-func (Server) AddChart(w http.ResponseWriter, r *http.Request) {
-	// TODO: interface to storage backend for creating
-	res := fmt.Sprintf("%v", r)
-	_ = json.NewEncoder(w).Encode(res)
+func (s Server) AddChart(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")  // TODO: set globally, or default?
+
+	// Set max header size (50MB)
+	r.Body = http.MaxBytesReader(w, r.Body, 50<<20) // TODO: set globally, or default?
+
+	// Limit parsed form size (32MB)
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(Error{
+			Code: new(int(http.StatusBadRequest)),
+			Message: new(string("Invalid form data")),
+		})
+		return
+	}
+	defer func() {
+		if r.MultipartForm != nil {
+			// Clean up temp files
+			r.MultipartForm.RemoveAll()
+		}
+	}()
+
+	// Extract chart name from form data
+	name := r.FormValue("name")
+
+	// Extract chart bytes from form data
+	f, fh, err := r.FormFile("chart")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(Error{
+			Code: new(int(http.StatusBadRequest)),
+			Message: new(string("Missing chart")),
+		})
+		return
+	}
+	defer f.Close()
+
+	// TODO: validate file type is .tgz
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(Error{
+			Code: new(int(http.StatusBadRequest)),
+			Message: new(string("Malformed file contents")),
+		})
+	}
+
+	// Construct runtime type for compatibility with generated types
+	file := &openapi_types.File{}
+	file.InitFromBytes(data, fh.Filename)
+
+	bytes, err := file.Bytes()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(Error{
+			Code: new(int(http.StatusBadRequest)),
+			Message: new(string("Malformed file upload")),
+		})
+	}
+
+	if err := s.storageHandler.Write(name, bytes); err != nil {  // TODO: return created version
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(Error{
+			Code: new(int(http.StatusInternalServerError)),
+			Message: new(string("Error occurred during file IO")),
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(Error{
+		Code: new(int(http.StatusCreated)),
+		Message: new(string(fmt.Sprintf("Chart '%v' created", name))),
+	})
 }
